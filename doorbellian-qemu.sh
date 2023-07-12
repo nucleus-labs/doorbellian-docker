01c1cad4fc22594ff7ddeae83b77a3192abf1e33
*Kim Zick (rummik) <k@9k1.us>
2023-07-12 13:32:15 -0400

    fix port forwarding to mediamtx

--- doorbellian-qemu.sh
+++ doorbellian-qemu.sh
@@ -26,15 +26,5 @@ qemu-system-riscv64 \
     -drive file=busybox-disk,format=raw,id=hd0 \
     -device virtio-blk-device,drive=hd0 \
     \
-    -netdev user,id=eth3,hostfwd=tcp::1935-:1935 \
     -device virtio-net-device,netdev=eth0 \
-    -netdev user,id=eth1,hostfwd=tcp::8000-:8000 \
-    -device virtio-net-device,netdev=eth1 \
-    -netdev user,id=eth2,hostfwd=tcp::8001-:8001 \
-    -device virtio-net-device,netdev=eth2 \
-    -netdev user,id=eth0,hostfwd=tcp::8554-:8554 \
-    -device virtio-net-device,netdev=eth3 \
-    -netdev user,id=eth4,hostfwd=tcp::8888-:8888 \
-    -device virtio-net-device,netdev=eth4 \
-    -netdev user,id=eth5,hostfwd=tcp::8889-:8889 \
-    -device virtio-net-device,netdev=eth5
+    -netdev user,id=eth0,hostfwd=tcp::1935-:1935,hostfwd=tcp::8000-:8000,hostfwd=tcp::8001-:8001,hostfwd=tcp::8554-:8554,hostfwd=tcp::8888-:8888,hostfwd=tcp::8889-:8889
