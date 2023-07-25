7e95c89f7ffe6107cd6ef8a07b03a829135b181e
*Kim Zick (rummik) <k@9k1.us>
2023-07-25 03:28:35 -0400

    use cam instead of v4l2-ctl, add multi camera, etc

--- dhelper.sh
+++ dhelper.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/usr/bin/env bash
+# set -x
 
 # ================================================================================================
 
@@ -29,49 +30,29 @@ fi
 # ================================================================================================
 
 # Store video devices
-v4l2_output=$(v4l2-ctl --list-devices)
+cam_output=$(cam -l 2>/dev/null)
 
 # Find device entries like "UVC Camera (046d:0825) (usb-0000:00:14.0-3)"
 
-matches=$(echo "$v4l2_output" | grep -oE '\([0-9a-fA-F:]+\)')
-ids=""
-
-# Loop through each match
-while read -r match; do
-    # Look for the end of the match so the contents can be searched
-    match_end=$(echo "$v4l2_output" | grep -n "$match" | awk -F: '{print $1}' | tail -n1)
-    devices=$(echo "$v4l2_output" | sed -n "/$match/,/$match_end/ p" | grep -E '(/\dev/media[0-9]+|\dev/video[0-9]+)')
-
-    # Check if match found
-    if [ -n "$devices" ]; then
-        # Find the ID from the match
-        id=$(echo "$match" | grep -oE '\([0-9a-fA-F:]+\)' | tr -d '()')
-        ids="${ids} ${id}"
-    fi
-done <<< "$matches"
-
-ids=$(echo "$ids" | tr -d ' ')
-
-# highlight /dev/video* and /dev/media* in yellow
-v4l2_output=$(echo "$v4l2_output" | sed -E 's#/dev/(media[^[:space:]]*|video[^[:space:]]*)#\o033[43;30m&\o033[0m#g')
+ids=$(echo "$cam_output" | grep -oE '[0-9a-f]{4}:[0-9a-f]{4}' | uniq)
+ids=$(echo -n "$ids" | tr "\n" "|")
 
 # highlight the device IDs in pink
-v4l2_output=$(echo "$v4l2_output" | sed -E "s@($ids)@\o033[45;30m&\o033[0m@g")
+cam_output=$(echo "$cam_output" | sed -E "s@($ids)@\o033[45;30m&\o033[0m@g")
 
 lsusb_output=$(lsusb)
 
 
-bus_ids=$(echo "$lsusb_output" | sed -En "s/Bus ([0-9]{3}) Device ([0-9]{3}): ID ($ids).*/\1/p" | awk '{ printf "%d", $1 }')
-bus_addrs=$(echo "$lsusb_output" | sed -En "s/Bus ([0-9]{3}) Device ([0-9]{3}): ID ($ids).*/\2/p" | awk '{ printf "%d", $1 }')
+bus_ids=$(echo "$lsusb_output" | sed -En "s/Bus ([0-9]{3}) Device ([0-9]{3}): ID ($ids).*/\1/p" | awk '{ printf "%d,", $1 }' | head -c -1)
+bus_addrs=$(echo "$lsusb_output" | sed -En "s/Bus ([0-9]{3}) Device ([0-9]{3}): ID ($ids).*/\2/p" | awk '{ printf "%d,", $1 }' | head -c -1)
 
 lsusb_output=$(echo "$lsusb_output" | sed -E "s/Bus ([0-9]{3}) Device ([0-9]{3}): ID ($ids)/Bus \x1b[46;30m\1\x1b[0m Device \x1b[42;30m\2\x1b[0m: ID \3/g")
 lsusb_output=$(echo "$lsusb_output" | sed -E "s@($ids)@\o033[45;30m&\o033[0m@g")
 
 
-
 if [ "$target" = "usb" ]; then
     echo ==============================================================
-    echo -e "$v4l2_output"
+    echo -e "$cam_output"
     echo ==============================================================
     echo -e "$lsusb_output"
     echo ==============================================================
@@ -80,7 +61,7 @@ if [ "$target" = "usb" ]; then
     echo "$matches"
 
     # Print the extracted IDs
-    echo "Extracted Device IDs:    ${ids}"
+    echo "Extracted Device IDs:    ${ids//|/,}"
     echo "Extracted USB Bus IDs:   ${bus_ids}"
     echo "Extracted USB Addresses: ${bus_addrs}"
 
@@ -93,32 +74,35 @@ fi
 
 if [ "$target" = "run" ] || [ "$target" = "bar" ] || [ "$target" = "bash" ]; then
 
-    if [ ! -e "/dev/media0" ] || [ ! -e "/dev/video0" ]; then
-        echo could not find /dev/media0 or /dev/video0
-        exit 3
-    fi
-
     if [ "$target" = "bash" ]; then
         container_cmd="/bin/bash"
     else
         container_args="$bus_ids $bus_addrs"
     fi
 
+    device_args=""
+    for dev in $(ls /dev/{video,media}* 2>/dev/null); do
+        device_args="$device_args --device=$dev"
+    done
+
+    if [[ -z "$device_args" ]]; then
+        echo "No video devices found."
+        exit 3
+    fi
+
     docker run -it --rm \
-	--privileged \
-	\
+        --privileged \
+        \
         --device=/dev/bus/usb:/dev/bus/usb \
-        --device=/dev/media0 \
-        --device=/dev/video0 \
-        --device=/dev/video1 \
-	\
+        $device_args \
+        \
         -p 1935:1935 \
         -p 8000:8000 \
         -p 8001:8001 \
         -p 8554:8554 \
         -p 8888:8888 \
         -p 8889:8889 \
-	\
+        \
         doorbellian:dev $container_cmd $container_args
 
     
