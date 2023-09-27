8c9734f9c4b25ef25fb205ed3b647e85d1f8aa94
Maxine Alexander <25956545+MaxTheMooshroom@users.noreply.github.com>
2023-09-27 13:29:33 -0400

    successful changes

--- deps/build/build-tina.sh
+++ deps/build/build-tina.sh
@@ -2,10 +2,8 @@
 
 
 export  FORCE_UNSAFE_CONFIGURE=1
-source  build/envsetup.sh           \
-    &&  lunch 1                     \
-    &&  make ARCH=riscv .config     \
-    &&  make ARCH=riscv -j${JOBS}
-#     &&  mboot                       \
-#     &&  pack
+source  build/envsetup.sh
+lunch d1_mq_pro-tina
 
+make ARCH=riscv .config
+make ARCH=riscv -j${JOBS}
